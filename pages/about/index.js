27837e8c280b4d8128b7f46783bc17f77b262169
OsamaQayyum <116727512+OsamaQayyum110@users.noreply.github.com>
2023-10-31 11:30:07 +0500

    Update index.js

--- pages/about/index.js
+++ pages/about/index.js
@@ -213,15 +213,15 @@ const About = () => {
           className="flex flex-col w-full xl:max-w-[48%] h-[480px]"
         >
           <div className="flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4">
-            {aboutData.map((item, itemIndex) => {
+            {aboutData.map((item, index) => {
               return (
                 <div
-                  key={itemIndex}
+                  key={index}
                   className={` ${
-                    index === itemIndex &&
+                    index === index &&
                     " text-[#28e98c] after:w-[100%] after:bg-[#28e98c] after:transition-all after:duration-300"
                   } cursor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px] after:bg-white after:absolute after:-bottom-1 after:left-0`}
-                  onClick={() => setIndex(itemIndex)}
+                  onClick={() => setIndex(index)}
                 >
                   {item.title}
                 </div>
@@ -229,10 +229,10 @@ const About = () => {
             })}
           </div>
           <div className="py-2 xl:py-6 flex flex-col gap-y-2 xl:gap-y-4 items-center xl:items-start">
-            {aboutData[index].info.map((item, itemIndex) => {
+            {aboutData[index].info.map((item, index) => {
               return (
                 <div
-                  key={itemIndex}
+                  key={index}
                   className="flex-1 flex flex-col md:flex-row max-w-max gap-x-2 items-center text-white/60"
                 >
                   {/* title */}
@@ -243,7 +243,7 @@ const About = () => {
                   <div className="flex gap-x-4">
                     {/* icons */}
 
-                    {item.icons?.map((icon, itemIndex) => {
+                    {item.icons?.map((icon, index) => {
                       return <div className="text-2xl text-white">{icon}</div>;
                     })}
                   </div>
